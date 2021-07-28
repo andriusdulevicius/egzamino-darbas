e3d2defb9976b97ce6ecfb5c54392ca4b3737b87
AndriusDulevicius <andriusdulevicius@email.com>
2021-07-28 09:14:40 +0300

    Users model created

---
+++ models/User.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    password2: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model('UsersData', userSchema);
+
+module.exports = User;
